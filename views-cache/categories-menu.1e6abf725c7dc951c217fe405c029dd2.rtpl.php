927f875811bafd9448609c1644cb2e1114bc9cb7
Samuel Ottoni <samuel.ottm@gmail.com>
2019-01-11 16:52:10 -0200

    Detalhes do Produto

--- views-cache/categories-menu.1e6abf725c7dc951c217fe405c029dd2.rtpl.php
+++ views-cache/categories-menu.1e6abf725c7dc951c217fe405c029dd2.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?> <li><a href="/categories/5">Apple</a></li> <li><a href="/categories/4">Google</a></li> <li><a href="/categories/6">Motolora</a></li> <li><a href="/categories/7">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?> <li><a href="/categories/4">Android</a></li> <li><a href="/categories/5">Apple</a></li> <li><a href="/categories/6">Motolora</a></li> <li><a href="/categories/7">Samsung</a></li>
