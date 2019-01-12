082390909a42895b552dfd0ca4803731b029e80d
Samuel Ottoni <samuel.ottm@gmail.com>
2019-01-12 18:45:29 -0200

    Carrinho de compras

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Category;
 use \Hcode\Model\Product;
+use \Hcode\Model\Cart;
 
 $app->get('/', function() {
 
@@ -59,6 +60,18 @@ $app->get("/products/:desurl",function($desurl){
 		'product'=>$product->getValues(),
 		'categories'=>$product->getCategories()
 	]);
+});
+
+$app->get("/cart",function(){
+
+	$cart = Cart::getFromSession();
+
+
+	$page = new Page();
+
+	$page->setTpl("cart");
+
+
 });
 
 
